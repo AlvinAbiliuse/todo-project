ebf612a8d736914cf47e3e0152838098b693b26c
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-17 06:28:25 +0400

    added functions to add card to dom

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
-console.log("hello world");
 
-
-
-class newData{
+class newData {
 	constructor(title, description, dueDate, priority, check) {
-			this title = title,
+			this.title = title,
 			this.description = description,
 			this.dueDate = dueDate,
 			this.priority = priority,
-			this.check = check,
+			this.check = check
 	}
 
-	return {title, description, dueDate, priority, check}
 };
 
+function createCard(data) {
+	const mainContainer = document.querySelector(".mainCards");
+	const newCard = document.createElement("div");
+	newCard.className = "card"
 
-console.log(new NewDate("todo list", "create a to do list",
-	"1 week", "important", "no"));
+	const title = document.createElement("h2");
+	const description = document.createElement("p");
+	const dueDate = document.createElement("p");
+	const priority = document.createElement("p");
+	const check = document.createElement("p");
+
+	title.textContent = data.title;
+	description.textContent = data.description;
+	dueDate.textContent = data.dueDate;
+	priority.textContent = data.priority;
+	check.textContent = data.check;
+
+	newCard.appendChild(title);		
+	newCard.appendChild(description);		
+	newCard.appendChild(dueDate);		
+	newCard.appendChild(priority);		
+	newCard.appendChild(check);		
+
+	mainContent.appendChild(newCard);
+
+}
+
+createCard(new newData("todo list", "create a to do list", "1 week", "important", "no"));
