72d5c26c5efabd286c31d36dca866cddf5fdf545
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-17 08:29:31 +0400

    progress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import listItems from "./data.json";
-
+import createCard from "./createCard.js";
 console.log(listItems);
 
 class newData {
@@ -11,40 +11,7 @@ class newData {
 			this.priority = priority,
 			this.check = check
 	}
-
 };
-
-function createCard(data, id) {
-	const mainCards = document.querySelector(".mainCards");
-	const newCard = document.createElement("div");
-	newCard.className = "card"
-
-	const title = document.createElement("h2");
-	const description = document.createElement("p");
-	const dueDate = document.createElement("p");
-	const priority = document.createElement("p");
-	const check = document.createElement("p");
-	const identifier = document.createElement("p");
-
-	title.textContent = data.title;
-	description.textContent = data.description;
-	dueDate.textContent = data.dueDate;
-	priority.textContent = data.priority;
-	check.textContent = data.check;
-	identifier.textContent = id;
-	identifier.className = "identifier";
-
-	newCard.appendChild(title);		
-	newCard.appendChild(description);		
-	newCard.appendChild(dueDate);		
-	newCard.appendChild(priority);		
-	newCard.appendChild(check);
-	newCard.appendChild(identifier);
-	console.log(listItems[identifier.textContent]);
-
-	mainCards.appendChild(newCard);
-
-}
 
 function initializePage() {
 	for (let i = 0; i < Object.keys(listItems).length; i++) {
@@ -56,17 +23,6 @@ function initializePage() {
 }
 
 initializePage();
-
-console.log(JSON.stringify(listItems));
-
-
-
-
-
-
-
-
-
 
 
 
