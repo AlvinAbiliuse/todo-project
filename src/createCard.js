5b4f02deb5452e23b11621916c294b3a12609261
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-17 08:30:30 +0400

    made createCard it's own module

---
+++ src/createCard.js
@@ -0,0 +1,32 @@
+const createCard = (data, id) => {
+  const mainCards = document.querySelector(".mainCards");
+  const newCard = document.createElement("div");
+  newCard.className = "card"
+
+  const title = document.createElement("h2");
+  const description = document.createElement("p");
+  const dueDate = document.createElement("p");
+  const priority = document.createElement("p");
+  const check = document.createElement("p");
+  const identifier = document.createElement("p");
+
+  title.textContent = data.title;
+  description.textContent = data.description;
+  dueDate.textContent = data.dueDate;
+  priority.textContent = data.priority;
+  check.textContent = data.check;
+  identifier.textContent = id;
+  identifier.className = "identifier";
+
+  newCard.appendChild(title);   
+  newCard.appendChild(description);   
+  newCard.appendChild(dueDate);   
+  newCard.appendChild(priority);    
+  newCard.appendChild(check);
+  newCard.appendChild(identifier);
+
+  mainCards.appendChild(newCard);
+
+}
+
+export default createCard;
