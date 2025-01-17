f19b726b61c7930c3a7b58db5a12a586bda1e433
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-17 04:42:00 +0400

    added a class and basic console log to check if it's working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log("hello world");
+
+
+
+class newData{
+	constructor(title, description, dueDate, priority, check) {
+			this title = title,
+			this.description = description,
+			this.dueDate = dueDate,
+			this.priority = priority,
+			this.check = check,
+	}
+
+	return {title, description, dueDate, priority, check}
+};
+
+
+console.log(new NewDate("todo list", "create a to do list",
+	"1 week", "important", "no"));
