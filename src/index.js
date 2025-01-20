3e521111cb0874439c0b740cc986afee2ad061fc
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-20 07:18:00 +0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+let storage = window["localStorage"];
+
+
+if (storage["storage"]) {
+	console.log("Storage Exists");
+} else {
+	console.log("Data does not exist; creating Storage");
+	storage.setItem("storage", JSON.stringify(
+	{
+		"Default": {
+			"name": "Default",
+			"cards": {
+			},
+		}
+	}))
+}
+
+let data = JSON.parse(storage["storage"]);
+console.log(data);
