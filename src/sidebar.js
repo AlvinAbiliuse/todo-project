494c5d140735315eb59030e3a8947a5b25ee254e
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-22 14:38:18 +0400

    added logo to webpage and head along with it's styling

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,11 +1,15 @@
+import logoImage from "./images/toDoList.png";
 
 export function sidebar(data) {
 	let sidebar = document.querySelector(".sidebar");
+	let logo = document.createElement("img");
 	let header = document.createElement("h1");
 	let newProjectBtn = document.createElement("button");
 	let modal = document.createElement("dialog");
 	let cardContainer = document.createElement("div");
-
+	
+	logo.setAttribute("src", logoImage);
+	logo.className = "logo";
 	header.textContent = "TODO";
 	newProjectBtn.className = "newProjectButton";
 	newProjectBtn.textContent = "+ New Project";
@@ -22,6 +26,7 @@ export function sidebar(data) {
 	modal.appendChild(projectName);
 	modal.appendChild(nameInput);
 	
+	sidebar.appendChild(logo);
 	sidebar.appendChild(header);
 	sidebar.appendChild(newProjectBtn);
 	sidebar.appendChild(modal);
